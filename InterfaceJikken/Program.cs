f825ad2d4849417fbd0635788ee841f0d85fc325
tera1707 <Masa1017>
2023-08-03 00:03:58 +0900

    パターン①②③を追加

--- InterfaceJikken/Program.cs
+++ InterfaceJikken/Program.cs
@@ -7,50 +7,30 @@ namespace InterfaceJikken
         static void Main(string[] args)
         {
             {
-                MyClass1 mc1 = new MyClass1();
-                MyInterface1 mi1 = new MyClass1();
-
-                //mc1.Method1_1();
-                //mc1.Method1_2();
-                //((MyInterface1)mc1).Method1_1();
-                //((MyInterface1)mc1).Method1_2();
-            }
-
-            {
-                MyClass2 mc2 = new MyClass2();
-                MyInterface2 mi2 = new MyClass2();
+                MyClass2 asClassInstance = new MyClass2();
+                MyInterface2 asInterface = new MyClass2();
 
                 Debug.WriteLine("mc2");
-                mc2.Method1_1();
-                mc2.Method1_2();
+                asClassInstance.Method1_1();
 
                 Debug.WriteLine("mc2 class");
-                ((MyClass1)mc2).Method1_1();
-                ((MyClass1)mc2).Method1_2();
-                ((MyClass2)mc2).Method1_1();//不要なキャスト
-                ((MyClass2)mc2).Method1_2();//不要なキャスト
+                ((MyClass1)asClassInstance).Method1_1();
+                ((MyClass2)asClassInstance).Method1_1();
 
                 Debug.WriteLine("mc2 if");
-                ((MyInterface1)mc2).Method1_1();
-                ((MyInterface1)mc2).Method1_2();
-                ((MyInterface2)mc2).Method1_1();
-                ((MyInterface2)mc2).Method1_2();
+                ((MyInterface1)asClassInstance).Method1_1();
+                ((MyInterface2)asClassInstance).Method1_1();
 
                 Debug.WriteLine("mi2");
-                mi2.Method1_1();
-                mi2.Method1_2();
+                asInterface.Method1_1();
 
                 Debug.WriteLine("mi2 class");
-                ((MyClass1)mi2).Method1_1();
-                ((MyClass1)mi2).Method1_2();
-                ((MyClass2)mi2).Method1_1();
-                ((MyClass2)mi2).Method1_2();
+                ((MyClass1)asInterface).Method1_1();
+                ((MyClass2)asInterface).Method1_1();
 
                 Debug.WriteLine("mi2 if");
-                ((MyInterface1)mi2).Method1_1();//不要なキャスト
-                ((MyInterface1)mi2).Method1_2();//不要なキャスト
-                ((MyInterface2)mi2).Method1_1();//不要なキャスト
-                ((MyInterface2)mi2).Method1_2();//不要なキャスト
+                ((MyInterface1)asInterface).Method1_1();//不要なキャスト
+                ((MyInterface2)asInterface).Method1_1();//不要なキャスト
             }
         }
     }
@@ -58,23 +38,44 @@ namespace InterfaceJikken
     internal interface MyInterface1
     {
         void Method1_1();
-        void Method1_2();
     }
     internal interface MyInterface2 : MyInterface1
     {
-        void Method2_1();
+        // 実質、MyInterface1と同じものを持ったinterface
     }
-
+#if true // ①
     internal class MyClass1 : MyInterface1
     {
-        public virtual void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass1");  // ★ココが②と違う
-        public void Method1_2()=> Debug.WriteLine(" Method1_2 of MyClass1");
+        public void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass1");  // ①
     }
 
     internal class MyClass2 : MyClass1, MyInterface2
     {
-        public override void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass2");  // ★ココが②と違う
-        public void Method1_2() => Debug.WriteLine(" Method1_2 of MyClass2");
-        public void Method2_1() => Debug.WriteLine(" Method2_1 of MyClass2");
+        public new void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass2");  // ②
     }
+#endif
+
+#if false // ②
+    internal class MyClass1 : MyInterface1
+    {
+        public void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass1");  // ①
+    }
+
+    internal class MyClass2 : MyClass1, MyInterface2
+    {
+        void MyInterface1.Method1_1() => Debug.WriteLine(" Method1_1 of MyClass2");  // ②
+    }
+#endif
+
+#if false // ③
+    internal class MyClass1 : MyInterface1
+    {
+        public virtual void Method1_1() => Debug.WriteLine(" Method1_1 of MyClass1");  // ①
+    }
+
+    internal class MyClass2 : MyClass1, MyInterface2
+    {
+        public override void  Method1_1() => Debug.WriteLine(" Method1_1 of MyClass2");  // ②
+    }
+#endif
 }
